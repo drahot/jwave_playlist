acd5b29c7bf2a1c4a19a1baac0349f0a6215950b
hottin <hotterdragon@gmail.com>
2023-07-02 16:42:53 +0900

    update: ソング情報を取得する処理を追加

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,29 @@ import { chromium } from 'playwright-core'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const main = async () => {
-  const browser = await chromium.launch({ channel: 'chrome', headless: true })
+  const browser = await chromium.launch({
+    channel: 'chrome',
+    headless: true,
+    slowMo: 1000,
+  })
   const page = await browser.newPage()
-  await page.goto('https://www.google.com')
-  await page.screenshot({ path: './tmp/sample.png' })
+  await page.goto('https://www.j-wave.co.jp/songlist/')
+  const list = await page.evaluate(() => {
+    const songInfos = document.querySelectorAll(
+      '.list_songs > .song > .song_wrap > .song_inner > .col_l > .song_info'
+    )
+
+    return Array.from(songInfos.values()).map((songInfo) => {
+      const songName = songInfo.querySelector('h4')?.textContent ?? ''
+      const artistName =
+        songInfo.querySelector('.txt_artist > span')?.textContent ?? ''
+      return {
+        songName,
+        artistName,
+      }
+    })
+  })
+  console.log(list)
   await browser.close()
 }
 
