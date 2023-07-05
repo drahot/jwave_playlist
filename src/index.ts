c6dd863b3e4192032daab295b792c5684ea15c37
hottin <hotterdragon@gmail.com>
2023-07-05 21:55:21 +0900

    update: j-waveオンエア曲取得処理は外だし

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-import { chromium } from 'playwright-core'
+import { getOnAirList } from './lib/jwave'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const main = async () => {
-  const browser = await chromium.launch({
-    channel: 'chrome',
-    headless: true,
-    slowMo: 500,
-  })
-  const page = await browser.newPage()
-  await page.goto('https://www.j-wave.co.jp/songlist/')
-  const list = await page.evaluate(() => {
-    const songInfos = document.querySelectorAll(
-      '.list_songs > .song > .song_wrap > .song_inner > .col_l > .song_info'
-    )
-    return Array.from(songInfos.values()).map((songInfo) => {
-      const songName = songInfo.querySelector('h4')?.textContent ?? ''
-      const artistName =
-        songInfo.querySelector('.txt_artist > span')?.textContent ?? ''
-      return {
-        songName,
-        artistName,
-      }
-    })
-  })
+const main = async (): Promise<void> => {
+  const list = await getOnAirList()
   console.log(list)
-  await browser.close()
 }
 
-// eslint-disable-next-line
-main().then(console.log).then(console.error)
+void main().then(console.log).then(console.error)
