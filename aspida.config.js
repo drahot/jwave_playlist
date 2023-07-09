7c1dfe9be27ad7a3bcd3d257e404fd77f1fcb991
hottin <hotterdragon@gmail.com>
2023-07-09 22:41:19 +0900

    update: configの変更

--- aspida.config.js
+++ aspida.config.js
@@ -1,9 +1,16 @@
-module.exports = {
-  input: 'spotify',
-  outputEachDir: true,
-  baseURL: 'https://api.spotify.com/v1',
-  openapi: {
-    inputFile:
-      'https://raw.githubusercontent.com/APIs-guru/openapi-directory/main/APIs/spotify.com/1.0.0/openapi.yaml',
+module.exports = [
+  {
+    input: 'spotify',
+    outputEachDir: true,
+    baseURL: 'https://api.spotify.com/v1',
+    openapi: {
+      inputFile:
+        'https://raw.githubusercontent.com/APIs-guru/openapi-directory/main/APIs/spotify.com/1.0.0/openapi.yaml',
+    },
   },
-}
+  {
+    input: 'spotify_auth',
+    outputEachDir: true,
+    baseURL: 'https://accounts.spotify.com/api',
+  },
+]
