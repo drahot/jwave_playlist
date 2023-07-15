a0fbf2340d7d3fc234d7d072552c20f2f383f07a
hottin <hotterdragon@gmail.com>
2023-07-15 22:03:34 +0900

    update: リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import { getOnAirList } from './lib/jwave'
 import { authenticate } from './lib/spotify_auth'
+import * as process from 'process'
 
 const main = async () => {
   const authResult = await authenticate()
 
   if (authResult.error) {
     console.error(authResult.error.message)
-    return
+    process.exit(1)
   }
 
   if (!authResult.auth) {
     console.error('authResult.auth is undefined')
-    return
+    process.exit(1)
   }
 
   const auth = authResult.auth
@@ -22,6 +23,7 @@ const main = async () => {
   const list = await getOnAirList()
 
   console.log(list)
+  process.exit()
 }
 
 main().then(console.log).then(console.error)
