9a7145512112098056cb4b9cfeb1a530dd809903
hottin <hotterdragon@gmail.com>
2023-08-13 15:57:26 +0900

    一旦コミット

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { authenticate } from './lib/spotify_auth'
+import { authorize } from './lib/spotify_auth'
 import * as process from 'process'
-import { getOnAirList, Song } from './lib/jwave'
+import { Song } from './lib/jwave'
 import { spotify } from './lib/spotify'
 import dayjs from 'dayjs'
 
@@ -70,7 +70,7 @@ const createPlaylist = async (client: spotifyClient, trackUris: string[]) => {
 }
 
 const main = async () => {
-  const authResult = await authenticate()
+  const authResult = await authorize()
 
   if (authResult.error) {
     console.error(authResult.error.message)
@@ -82,18 +82,20 @@ const main = async () => {
     process.exit(1)
   }
 
-  const auth = authResult.data
-  console.log('auth_token: ', auth.access_token)
-  console.log('expires_in: ', auth?.expires_in)
+  // const auth = authResult.data
+  // console.log('auth_token: ', auth.access_token)
+  // console.log('expires_in: ', auth?.expires_in)
 
-  const client = spotify(auth.access_token)
-
-  const songs = await getOnAirList()
-
-  const trackUris = await searchTracks(client, songs.slice(0, 50))
-  console.log(trackUris.length)
-  const playlist = await createPlaylist(client, trackUris)
-  console.log(playlist?.external_urls?.spotify)
+  const authToken = authResult.data
+  console.log(authToken)
+  // const client = spotify(authToken)
+  //
+  // const songs = await getOnAirList()
+  //
+  // const trackUris = await searchTracks(client, songs.slice(0, 50))
+  // console.log(trackUris.length)
+  // const playlist = await createPlaylist(client, trackUris)
+  // console.log(playlist?.external_urls?.spotify)
   process.exit()
 }
 
--- src/lib/spotify_auth.ts
+++ src/lib/spotify_auth.ts
@@ -8,7 +8,7 @@ import { Result } from './result'
 
 env.config()
 
-export const authenticate = async (): Promise<Result<AuthResult>> => {
+export const token = async (): Promise<Result<AuthResult>> => {
   if (!process.env.SPOTIFY_CLIENT_ID) {
     return { data: undefined, error: new Error('SPOTIFY_CLIENT_ID is not set') }
   }
@@ -30,18 +30,61 @@ export const authenticate = async (): Promise<Result<AuthResult>> => {
 
   const client = api(aspida())
   try {
-    const { body, status } = await client.api.token.post({
+    const { body } = await client.api.token.post({
       body: {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials',
       },
     })
-    if (status !== 200) {
-      return { data: undefined, error: new Error('auth error') }
-    }
     return { data: body, error: undefined }
-  } catch (error: unknown) {
-    return { data: undefined, error: error as Error }
+  } catch (e) {
+    return { data: undefined, error: e as Error }
+  }
+}
+
+const BASE_CHARS =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+const generateString = (length: number) => {
+  const chars = Array.from({ length }, () => {
+    const index = Math.floor(Math.random() * BASE_CHARS.length)
+    return BASE_CHARS[index]
+  })
+
+  return chars.join('')
+}
+
+export const authorize = async (): Promise<Result<string>> => {
+  if (!process.env.SPOTIFY_CLIENT_ID) {
+    return { data: undefined, error: new Error('SPOTIFY_CLIENT_ID is not set') }
+  }
+  const clientId = process.env.SPOTIFY_CLIENT_ID ?? ''
+  const state = generateString(16)
+
+  const client = api(aspida())
+  try {
+    const { body } = await client.authorize.get({
+      body: {
+        client_id: clientId,
+        response_type: 'code',
+        state: state,
+        redirect_uri: 'http://localhost',
+        scope: 'user-read-currently-playing playlist-modify-private',
+      },
+    })
+    const redirectUri = new TextDecoder().decode(body)
+    const url = new URL(redirectUri)
+    if (state !== url.searchParams.get('state') ?? '') {
+      return {
+        data: undefined,
+        error: new Error('state is not matched'),
+      }
+    }
+    const code = url.searchParams.get('code') ?? ''
+    return { data: code, error: undefined }
+  } catch (e) {
+    console.log(e)
+    return { data: undefined, error: e as Error }
   }
 }
