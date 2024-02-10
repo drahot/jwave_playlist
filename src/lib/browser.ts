1595d936fcc888e5e9c47740b6ca9948892ada52
hottin <hotterdragon@gmail.com>
2024-02-10 23:18:33 +0900

    不要な関数定義を削除

--- src/lib/browser.ts
+++ src/lib/browser.ts
@@ -1,7 +1,4 @@
 import { chromium } from 'playwright-core'
-import { Song } from './types'
-
-export type GetSongs = () => Promise<Song[]>
 
 export const getBrowser = async (url: string) => {
   const browser = await chromium.launch({
