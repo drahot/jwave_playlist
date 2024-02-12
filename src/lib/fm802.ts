1b9fc43108feafda7f2e81026dd1aab14b25231c
hottin <hotterdragon@gmail.com>
2024-02-12 23:01:31 +0900

    FM802のScrapingコードを追加

--- src/lib/fm802.ts
+++ src/lib/fm802.ts
@@ -1,5 +1,27 @@
-import { Song } from './jwave'
+import { Song } from './types'
+import { getBrowser } from './browser'
+
+const FM802_ON_AIR_URL = 'https://funky802.com/site/onairlist'
 
 export const getOnAirList = async (): Promise<Song[]> => {
-  return []
+  const { browser, page } = await getBrowser(FM802_ON_AIR_URL)
+
+  try {
+    return await page.evaluate(() => {
+      const songInfos = document.querySelectorAll('.c-infoOnair__list')
+      return Array.from(songInfos.values()).map((songInfo) => {
+        const artistName =
+          songInfo.querySelector('.c-infoOnair__list--artist')?.textContent ??
+          ''
+        const songName =
+          songInfo.querySelector('.c-infoOnair__list--title')?.textContent ?? ''
+        return {
+          songName,
+          artistName,
+        }
+      })
+    })
+  } finally {
+    await browser.close()
+  }
 }
