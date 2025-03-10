94215ad65e1936f7052ef28d8b6867636aab0a77
hottin <hotterdragon@gmail.com>
2025-03-10 21:41:29 +0900

    ライブラリアップデート

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,153 +1,153 @@
 // noinspection JSUnresolvedReference
 
-import aspida from '@aspida/fetch'
-import * as searchApi from '../../spotify/search/$api'
-import * as usersApi from '../../spotify/users/$api'
-import * as playlistsApi from '../../spotify/playlists/$api'
-import * as meApi from '../../spotify/me/$api'
+import aspida from "@aspida/fetch";
+import * as searchApi from "../../spotify/search/$api";
+import * as usersApi from "../../spotify/users/$api";
+import * as playlistsApi from "../../spotify/playlists/$api";
+import * as meApi from "../../spotify/me/$api";
 
 import {
-  PagingPlaylistObject,
-  PagingPlaylistTrackObject,
+  PagingObject,
   PlaylistObject,
+  PlaylistTrackObject,
   PrivateUserObject,
-  TrackObject,
-} from '../../spotify/@types'
-import { Result } from 'result-type-ts'
-import * as process from 'process'
+  TrackObject
+} from "../../spotify/@types";
+import { Result } from "result-type-ts";
+import * as process from "process";
 
-const userId = process.env.SPOTIFY_USER_ID ?? ''
+const userId = process.env.SPOTIFY_USER_ID ?? "";
 
 export const spotify = (accessToken: string) => {
   const config = {
     headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  }
+      Authorization: `Bearer ${accessToken}`
+    }
+  };
 
   const getResult = <T>(callback: () => Promise<T>) =>
     callback()
       .then((data) => Result.success(data))
-      .catch((e) => Result.failure(e))
+      .catch((e) => Result.failure(e));
 
   const partialMatch = (artistName: string, artist: string) => {
-    const matchLength = Math.round(artist.length * 0.8)
-    const partialArtistName = artistName.slice(0, matchLength)
-    return artistName.startsWith(partialArtistName)
-  }
+    const matchLength = Math.round(artist.length * 0.8);
+    const partialArtistName = artistName.slice(0, matchLength);
+    return artistName.startsWith(partialArtistName);
+  };
 
   const splitMatch = (artistName: string, artist: string) => {
-    const splitString = ['/', 'feat.', 'with', '&', 'and']
+    const splitString = ["/", "feat.", "with", "&", "and"];
     for (const str of splitString) {
       if (artist.includes(str)) {
-        const [name] = artist.split(str)
+        const [name] = artist.split(str);
         if (artistName === name.trim()) {
-          return true
+          return true;
         }
       }
     }
-    return false
-  }
+    return false;
+  };
 
   return {
     // トラックを検索する
     searchTrack: async (
       artist: string,
       song: string
-    ): Promise<Result<TrackObject[], Error>> =>
+    ): Promise<TrackObject[]> =>
       getResult(async () => {
-        const client = searchApi.default(aspida())
+        const client = searchApi.default(aspida());
 
         const query = `remaster track:${song} artist:${artist}`.replace(
           / /g,
-          '%20'
-        )
+          "%20"
+        );
         const data = await client.get({
-          query: { q: query, type: ['track'], limit: 50 },
-          config: config,
-        })
+          query: { q: query, type: ["track"], limit: 50 },
+          config: config
+        });
 
         return (data.body.tracks.items ?? []).filter((item) => {
           if ((item.album?.artists.length ?? 0) === 0) {
-            return false
+            return false;
           }
-          const target = artist.toLowerCase()
+          const target = artist.toLowerCase();
 
           for (const artist of item.album?.artists ?? []) {
-            const artistName = artist.name?.toLowerCase() ?? ''
+            const artistName = artist.name?.toLowerCase() ?? "";
 
             if (
               artistName === target ||
               partialMatch(artistName, target) ||
               splitMatch(artistName, target)
             ) {
-              return item.name?.toLowerCase().startsWith(song.toLowerCase())
+              return item.name?.toLowerCase().startsWith(song.toLowerCase());
             }
           }
-          return false
-        })
+          return false;
+        });
       }),
     // プレイリストを作成する
     createPlaylist: async (
       name: string,
       description?: string,
       isPublic = false
-    ): Promise<Result<PlaylistObject, Error>> =>
+    ): Promise<PlaylistObject> =>
       getResult(async () => {
-        const client = usersApi.default(aspida())
+        const client = usersApi.default(aspida());
         const data = await client._user_id(userId).playlists.post({
-          body: { name, description: description ?? '', public: isPublic },
-          config: config,
-        })
-        return data.body
+          body: { name, description: description ?? "", public: isPublic },
+          config: config
+        });
+        return data.body;
       }),
     // カレントユーザーのプレイリストを取得する
     getUserPlaylists: async (
       offset = 0,
       limit = 50
-    ): Promise<Result<PagingPlaylistObject, Error>> =>
+    ): Promise<PagingObject & { items?: SimplifiedPlaylistObject[] | undefined }> =>
       getResult(async () => {
-        const client = usersApi.default(aspida())
+        const client = usersApi.default(aspida());
         const data = await client._user_id(userId).playlists.get({
           query: { offset, limit },
-          config: config,
-        })
-        return data.body
+          config: config
+        });
+        return data.body;
       }),
     getPlaylistTracks: async (
       playlistId: string,
       offset = 0,
       limit = 50
-    ): Promise<Result<PagingPlaylistTrackObject, Error>> =>
+    ): Promise<PagingObject & { items?: PlaylistTrackObject[] | undefined }> =>
       getResult(async () => {
-        const client = playlistsApi.default(aspida())
+        const client = playlistsApi.default(aspida());
         const data = await client._playlist_id(playlistId).tracks.get({
           query: { offset, limit },
-          config: config,
-        })
-        return data.body
+          config: config
+        });
+        return data.body;
       }),
     // プレイリストに曲を追加
     addItemsToPlaylist: async (
       playlistId: string,
       trackUris: string[]
-    ): Promise<Result<{ snapshot_id: string }, Error>> =>
+    ): Promise<{ snapshot_id: string }> =>
       getResult(async () => {
-        const client = playlistsApi.default(aspida())
+        const client = playlistsApi.default(aspida());
         const data = await client._playlist_id(playlistId).tracks.post({
           body: { position: 0, uris: trackUris },
-          config: config,
-        })
-        return data.body
+          config: config
+        });
+        return data.body;
       }),
     // meを取得する
-    me: async (): Promise<Result<PrivateUserObject, Error>> =>
+    me: async (): Promise<PrivateUserObject> =>
       getResult(async () => {
-        const client = meApi.default(aspida())
+        const client = meApi.default(aspida());
         const data = await client.get({
-          config: config,
-        })
-        return data.body
-      }),
-  }
-}
+          config: config
+        });
+        return data.body;
+      })
+  };
+};
